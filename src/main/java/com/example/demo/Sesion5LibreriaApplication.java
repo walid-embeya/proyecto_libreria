fe270d71acda41861328ef72e8fd71d35c37c66d
walid <walid@yahoo.fr>
2023-03-07 12:42:01 +0100

    primer commit

---
+++ src/main/java/com/example/demo/Sesion5LibreriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class Sesion5LibreriaApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(Sesion5LibreriaApplication.class, args);
+	}
+
+}
